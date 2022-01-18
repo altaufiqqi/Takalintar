b5c8c50cb77976850c04092416bd0a529cc5e8f5
altaufiqqi <muhammad21taufiq21@mail.ugm.ac.id>
2022-01-18 18:02:26 +0700

    hapus comment

--- aset/index.js
+++ aset/index.js
@@ -1,60 +1,3 @@
-// const takalintar = {
-//     displayInitiate1: '-',
-//     displayInitiate2: '-',
-// };
-
-// const displayInit1 = document.querySelector("#displayInitiate1");
-// const displayInit2 = document.querySelector("#displayInitiate2");
-// const displayInit3 = document.querySelector("#displayInitiate3");
-// const displayInit4 = document.querySelector("#displayInitiate4");
-// const displayInit5 = document.querySelector("#displayInitiate5");
-// const displayInit6 = document.querySelector("#displayInitiate6");
-// const buttons = document.querySelectorAll(".button");
-// const clear = document.querySelector(".clear");
-// const clearAll = document.querySelector(".all-clear");
-
-
-
-// function updateDisplay1(){
-//     document.querySelector("#displayInitiate1").innerText = takalintar.displayInitiate1;
-// }
-
-// function updateDisplay2(){
-//     document.querySelector("#displayInitiate2").innerText = takalintar.displayInitiate2;
-// }
-
-// function clearTakalintar() {
-//     takalintar.displayInitiate1 = '-';
-//     takalintar.displayInitiate2 = '-';
-// }
-
-// function inputDigit(digit) {
-//     takalintar.displayInitiate1 = digit;
-// }
-
-// function inputDigit2(digit2){
-//     takalintar.displayInitiate2 = digit2;
-// }
-
-// for (let button of buttons) {
-//     button.addEventListener('click', function (event) {
-//         const target = event.target 
-
-//         if (target.classList.contains('clear')) {
-//             clearTakalintar();
-//             updateDisplay1();
-//             updateDisplay2();
-//             return;
-//         }
-
-//         inputDigit(target.innerText);
-//         inputDigit2(target.innerText);
-//         updateDisplay1();
-//         updateDisplay2();
-//         return;
-//     });
-// }
-
 const disInit = document.querySelectorAll('.displayInitiate');
 const disNum = document.querySelectorAll('.displayNumber');
 const disRes = document.querySelectorAll('.displayRes');
@@ -211,17 +154,3 @@ function clearValue(){
     document.getElementById('dispNum17').value = '';
     document.getElementById('dispNum18').value = '';
 }
-// buttons.forEach( button => {
-//     button.addEventListener('click', (e)=>{
-//       let disNum = "";
-      
-//       if(disNum === ""){
-//         disNum += e.target.innerText;
-//         displayInit1.innerText = disNum;
-//       }
-//       else if(disNum !== ""){
-//         disNum += e.target.innerText;
-//         displayInit2.innerText = disNum;
-//       }
-//     })
-//   })
